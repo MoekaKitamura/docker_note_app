3345d0f06d65334c09af0b4aae63a507423ab7b9
moeka kitamura <moeka.k76@gmail.com>
2022-10-22 22:53:46 +0900

    create note table

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module NotesApp
 
     config.generators do |g|
       g.assets false
+      g.helper false
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
